50a12b71e95cb6324f92b7cd399fc7dbd890b0d1
Nikolai Pawlowski <nikolaip@gmail.com>
2017-12-24 11:30:04 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -11,10 +11,10 @@ def select_value_and_count_of_most_prolific_species
   "SELECT species, count(*) FROM characters GROUP BY species ORDER BY count(*) DESC LIMIT 1;"
 end
 
-def select_name_and_series_subgenres_of_authors
-  "SELECT authors.name, subgenres.name FROM series JOIN authors ON series.author_id = authors.id
-  JOIN subgenres ON series.subgenre_id = subgenres.id;"
-end
+# def select_name_and_series_subgenres_of_authors
+#   "SELECT authors.name, subgenres.name FROM series JOIN authors ON series.author_id = authors.id
+#   JOIN subgenres ON series.subgenre_id = subgenres.id;"
+# end
 
 def select_name_and_series_subgenres_of_authors
   "SELECT Authors.name, Subgenres.name
